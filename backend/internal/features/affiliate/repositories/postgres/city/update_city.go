ed6ba35e1ca996adda9760c5ffee06daa64f6970
ScPro <muhamadievbogdan@gmail.com>
2026-06-03 04:40:31 +0500

    add city repository

---
+++ backend/internal/features/affiliate/repositories/postgres/city/update_city.go
@@ -0,0 +1,50 @@
+package city_postgres_repository
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	core_database_models "github.com/0ScPro0/affiliate-system/internal/core/database/models"
+	domain "github.com/0ScPro0/affiliate-system/internal/core/domain/entity"
+	core_transport_dto "github.com/0ScPro0/affiliate-system/internal/core/transport/dto"
+)
+
+func (r *CityRepository) UpdateCity(
+	ctx context.Context,
+	city core_transport_dto.UpdateCityRequest,
+) (domain.City, error) {
+	ctx, cancel := context.WithTimeout(ctx, r.pool.OperationTimeout())
+	defer cancel()
+	
+	query := `
+		UPDATE affiliate_system.cities
+		SET name = $2
+		WHERE id = $1
+		RETURNING id, name, created_at
+	`
+	
+	row := r.pool.QueryRow(ctx, query, city.ID, city.Name)
+
+	var cityModel core_database_models.CityModel
+	err := row.Scan(
+		&cityModel.ID,
+		&cityModel.Name,
+		&cityModel.CreatedAt,
+	)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return domain.City{}, fmt.Errorf("city with id %d not found", city.ID)
+		}
+		return domain.City{}, fmt.Errorf("update error: %w", err)
+	}
+
+	domainCity := domain.NewCity(
+		cityModel.ID,
+		cityModel.Name,
+		cityModel.CreatedAt,
+	)
+
+	return domainCity, nil
+}
