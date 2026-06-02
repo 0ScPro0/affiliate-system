41d5ed97b03a93942cec524ae527bee8f236c869
ScPro <muhamadievbogdan@gmail.com>
2026-06-03 04:40:03 +0500

    add city dto

---
+++ backend/internal/core/transport/dto/city.go
@@ -0,0 +1,18 @@
+package core_transport_dto
+
+import "time"
+
+type CreateCityRequest struct {
+	Name string
+}
+
+type UpdateCityRequest struct {
+	ID        int
+	Name      string
+}
+
+type CityResponse struct {
+	ID        int
+	Name      string
+	CreatedAt time.Time
+}
