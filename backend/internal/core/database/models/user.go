e988f61e55d7a453d01a7c8cbc5dc272481a1b0b
ScPro <muhamadievbogdan@gmail.com>
2026-06-04 21:04:08 +0500

    add user database model

---
+++ backend/internal/core/database/models/user.go
@@ -0,0 +1,12 @@
+package core_database_models
+
+import "time"
+
+type UserModel struct {
+	ID           int
+	UserName     *string
+	Email        string
+	PasswordHash string
+	IsAdmin      bool
+	CreatedAt    time.Time
+}
