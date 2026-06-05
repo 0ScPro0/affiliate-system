82fb18805546b9f2615efb383f0b41d1493821bf
ScPro <muhamadievbogdan@gmail.com>
2026-06-05 19:05:05 +0500

    update server main file

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"os/signal"
 	"syscall"
 
@@ -13,11 +12,29 @@ import (
 	"github.com/0ScPro0/affiliate-system/internal/core/logger"
 	core_http_middleware "github.com/0ScPro0/affiliate-system/internal/core/transport/http/middleware"
 	core_http_server "github.com/0ScPro0/affiliate-system/internal/core/transport/http/server"
+	auth_service "github.com/0ScPro0/affiliate-system/internal/features/affiliate/auth/service"
+	auth_transport_http "github.com/0ScPro0/affiliate-system/internal/features/affiliate/auth/transport/http"
+	category_postgres_repository "github.com/0ScPro0/affiliate-system/internal/features/affiliate/category/repository/postgres"
+	category_service "github.com/0ScPro0/affiliate-system/internal/features/affiliate/category/service"
+	category_transport_http "github.com/0ScPro0/affiliate-system/internal/features/affiliate/category/transport/http"
 	city_postgres_repository "github.com/0ScPro0/affiliate-system/internal/features/affiliate/city/repository/postgres"
 	city_service "github.com/0ScPro0/affiliate-system/internal/features/affiliate/city/service"
 	city_transport_http "github.com/0ScPro0/affiliate-system/internal/features/affiliate/city/transport/http"
+	partner_postgres_repository "github.com/0ScPro0/affiliate-system/internal/features/affiliate/partner/repository/postgres"
+	partner_service "github.com/0ScPro0/affiliate-system/internal/features/affiliate/partner/service"
+	partner_transport_http "github.com/0ScPro0/affiliate-system/internal/features/affiliate/partner/transport/http"
+	user_postgres_repository "github.com/0ScPro0/affiliate-system/internal/features/affiliate/user/repository/postgres"
+	user_service "github.com/0ScPro0/affiliate-system/internal/features/affiliate/user/service"
+	user_transport_http "github.com/0ScPro0/affiliate-system/internal/features/affiliate/user/transport/http"
+
+	_ "github.com/0ScPro0/affiliate-system/docs"
 )
 
+// @title       Affiliate System API
+// @version     1.0
+// @description Affiliate System API
+// @host        127.0.0.1:8080
+// @BasePath    /api/v1/
 func main() {
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
@@ -29,8 +46,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println(cfg.Database.DBUrl)
 
 	log, err := logger.NewLogger(cfg)
 	if err != nil {
@@ -47,14 +62,33 @@ func main() {
 	defer pool.Close()
 	log.Info("Database pool successfully initialized")
 
-	// City feature
+	// Repositories
+	userRepository := user_postgres_repository.NewUserRepository(pool)
 	cityRepository := city_postgres_repository.NewCityRepository(pool)
+	partnerRepository := partner_postgres_repository.NewPartnerRepository(pool)
+	categoryRepository := category_postgres_repository.NewCategoryRepository(pool)
+
+	// Services
 	cityService := city_service.NewCityService(cityRepository)
+	authService := auth_service.NewAuthService(cfg, userRepository)
+	partnerService := partner_service.NewPartnerService(partnerRepository)
+	categoryService := category_service.NewCategoryService(categoryRepository)
+	userService := user_service.NewUserService(userRepository)
+
+	// HTTP Handlers
 	cityHandler := city_transport_http.NewCityHTTPHandler(cityService)
+	authHandler := auth_transport_http.NewAuthHTTPHandler(authService)
+	partnerHandler := partner_transport_http.NewPartnerHTTPHandler(partnerService)
+	categoryHandler := category_transport_http.NewCategoryHTTPHandler(categoryService)
+	userHandler := user_transport_http.NewUserHTTPHandler(userService)
 
 	// Routes
 	apiVersionRouter := core_http_server.NewAPIVersionRouter(core_http_server.ApiVersion1)
 	apiVersionRouter.RegisterRoutes(cityHandler.Routes()...)
+	apiVersionRouter.RegisterRoutes(authHandler.Routes()...)
+	apiVersionRouter.RegisterRoutes(partnerHandler.Routes()...)
+	apiVersionRouter.RegisterRoutes(categoryHandler.Routes()...)
+	apiVersionRouter.RegisterRoutes(userHandler.Routes()...)
 
 	httpServer := core_http_server.NewHTTPServer(
 		cfg,
@@ -66,6 +100,7 @@ func main() {
 		core_http_middleware.Auth(cfg),
 	)
 	httpServer.RegisterAPIVersionRouter(*apiVersionRouter)
+	httpServer.RegisterSwagger()
 
 	if err := httpServer.Run(ctx); err != nil {
 		log.Error("HTTP server run error:", zap.Error(err))
